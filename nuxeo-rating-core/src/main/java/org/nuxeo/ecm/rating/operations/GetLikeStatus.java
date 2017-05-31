6e48b32e60b706e0f525e35621e976e9e84d72e8
Florent Guillaume <fguillaume@nuxeo.com>
2017-06-01 01:07:41 +0200

    NXP-22454: new method Blobs.createJSONBlob

--- nuxeo-rating-core/src/main/java/org/nuxeo/ecm/rating/operations/GetLikeStatus.java
+++ nuxeo-rating-core/src/main/java/org/nuxeo/ecm/rating/operations/GetLikeStatus.java
@@ -73,7 +73,7 @@ public class GetLikeStatus {
         }
 
         JSONObject json = JSONObject.fromObject(status.toMap());
-        return Blobs.createBlob(json.toString(), "application/json");
+        return Blobs.createJSONBlob(json.toString());
     }
 
 }
